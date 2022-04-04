43fc1df8694423bbe1180e8b8719f99ec3c6ada8
chenjingbang <2805478472@qq.com>
2022-04-04 20:34:53 +0800

    清空无关文件及代码

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 引入ElementUI
+import ElementUI from 'element-ui'
+// 引入css
+import 'element-ui/lib/theme-chalk/index.css'
+// 使用ElementUI
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
