1c9da1ddfe600b7a870f88be0e9093a2926046b7
chenjingbang <2805478472@qq.com>
2022-04-04 21:16:29 +0800

    安装less-load，登录页面设置背景颜色以及绘制一个居中的登录盒子

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="login_container">
+    <div class="login_box"></div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background: #2b4b6b;
+  height: 100%;
+}
+
+.login_box {
+  width: 450px;
+  height: 350px;
+  background-color: #fff;
+  // 圆角边框
+  border-radius: 3px;
+  // 绝对定位,页面位移
+  position: absolute;
+  // 左和顶部各偏移50%，横和竖各偏移50%
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,9 @@ import router from './router'
 import ElementUI from 'element-ui'
 // 引入css
 import 'element-ui/lib/theme-chalk/index.css'
+// 导入全局样式表
+import './assets/css/global.css'
+
 // 使用ElementUI
 Vue.use(ElementUI)
 Vue.config.productionTip = false
