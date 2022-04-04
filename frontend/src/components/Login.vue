aa51e2efc4c3e3f6d06dce5f613d47e5589921ca
chenjingbang <2805478472@qq.com>
2022-04-04 23:30:17 +0800

    实现登录发起请求，并提示验证结果，成功跳转至home组价

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="login_container">
-    <div class="login_box">
+  <div class='login_container'>
+    <div class='login_box'>
       <!--头像区域-->
-      <div class="avatar_box">
-        <img src="../assets/logo.png" alt />
+      <div class='avatar_box'>
+        <img src='../assets/logo.png' alt />
       </div>
 
       <!--登录表单区域-->
       <el-form
-        ref="loginFormRef"
-        label-width="0px"
-        :model="loginForm"
-        :rules="loginFormRules"
-        class="login_form">
+        ref='loginFormRef'
+        label-width='0px'
+        :model='loginForm'
+        :rules='loginFormRules'
+        class='login_form'>
         <!--用户名-->
-        <el-form-item prop="username">
-          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        <el-form-item prop='username'>
+          <el-input v-model='loginForm.username' prefix-icon='iconfont icon-user'></el-input>
         </el-form-item>
         <!--密码-->
-        <el-form-item prop="password">
+        <el-form-item prop='password'>
           <el-input
-            v-model="loginForm.password"
-            prefix-icon="iconfont icon-3702mima"
-            type="password"></el-input>
+            v-model='loginForm.password'
+            prefix-icon='iconfont icon-3702mima'
+            type='password'></el-input>
         </el-form-item>
         <!--按钮区域-->
-        <el-form-item class="btns">
-          <el-button type="primary" @click="login">登录</el-button>
-          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        <el-form-item class='btns'>
+          <el-button type='primary' @click='login'>登录</el-button>
+          <el-button type='info' @click='resetLoginForm'>重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -38,36 +38,58 @@
 export default {
   data () {
     return {
-      // 登录表单的数据绑定对象  
+      // 登录表单的数据绑定对象
       loginForm: {
-        username: "",
-        password: ""
+        username: '',
+        password: ''
       },
-      //表单的验证规则对象
+      // 表单的验证规则对象
       loginFormRules: {
-        //验证用户名是否合法
+        // 验证用户名是否合法
         username: [
-          { required: true, message: "请输入登录名称", trigger: "blur" },
-          { min: 3, max: 10, message: "长度在 3 到 10 个字符", trigger: "blur" }
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
         ],
-        //验证密码是否合法
+        // 验证密码是否合法
         password: [
-          { required: true, message: "请输入登录密码", trigger: "blur" },
-          { min: 6, max: 15, message: "长度在 6 到 15 个字符", trigger: "blur" }
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
         ]
       }
-    };
+    }
   },
   methods: {
-    //点击重置按钮，重置登录表单
+    // 点击重置按钮，重置登录表单
     resetLoginForm: function () {
-      this.$refs.loginFormRef.resetFields();
+      this.$refs.loginFormRef.resetFields()
     },
+    login () {
+      this.$refs.loginFormRef.validate(async valid => {
+        // 表单预登陆验证输入是否合法
+        // 验证失败直接return不发起请求
+        if (!valid) return
+
+        // 发起请求
+        const { data: res } = await this.$http.post('login', this.loginForm)
+
+        // 对返回结果做判断，做弹框提示
+        if (res.meta.status !== 200) {
+          return this.$message.error('登录失败！请重新登录')
+        } else this.$message.success('登录成功！')
+
+        // 1.将登陆成功之后的token，保存到客户端的sessionStorage中
+        // 1.1 项目中除了登录之外的其他API接口，必须在登录之后才能访问
+        // 1.2 token只应在当前网站打开期间生效，所以将token保存在sessionStorage中
+        window.sessionStorage.setItem('token', res.data.token)
+        // 2.通过编程式导航跳转到后台主页，路由地址是 /home
+        this.$router.push('/home')
+      })
+    }
   }
 }
 </script>
 
-<style lang="less" scoped>
+<style lang='less' scoped>
 .login_container {
   background-color: #2b4b6b;
   height: 100%;
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 // 引入ElementUI
 import ElementUI from 'element-ui'
+// 导入axios
+import axios from 'axios'
 // 引入css
 import 'element-ui/lib/theme-chalk/index.css'
 // 导入全局样式表
@@ -12,6 +14,11 @@ import './assets/fonts/iconfont.css'
 
 // 使用ElementUI
 Vue.use(ElementUI)
+
+// 设置请求根路径
+axios.defaults.baseURl = 'http://127.0.0.1:8080/api'
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
