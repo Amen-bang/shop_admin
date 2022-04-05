ceaa386887b982374509e7a8ddc86f134b625420
chenjingbang <2805478472@qq.com>
2022-04-05 15:26:04 +0800

    实现头部区域的布局优化

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -2,6 +2,10 @@
   <el-container class="home-container">
     <!--头部区域-->
     <el-header>
+      <div>
+        <img class="logo_img" src="../assets/logo2.png" alt />
+        <span>陈邦邦后台管理系统</span>
+      </div>
       <el-button type="info" @click="loginOut">退出</el-button>
     </el-header>
     <!--页面主题区域-->
@@ -30,8 +34,26 @@ export default {
 .home-container {
   height: 100%;
 }
+.logo_img {
+  height: 55px;
+  width: 55px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+}
 .el-header {
   background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
 }
 .el-aside {
   background-color: #333744;
