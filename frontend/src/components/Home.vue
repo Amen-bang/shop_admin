b01e9b6f206d147ec1196edee4422eef3cda3df1
chenjingbang <2805478472@qq.com>
2022-04-05 11:02:27 +0800

    实现退出功能，清空token，令其自动跳转到登录页

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,9 +1,20 @@
 <template>
-  <div>这是主页</div>
+  <div>这是主页
+    <el-button type='info' @click='logout'>退出</el-button>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    loginOut: function () {
+      // 清空token
+      window.sessionStorage.clear()
+      // 跳转到登录页
+      this.$router.push('/login')
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
