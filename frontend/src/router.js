45cb85884f88ae76d7b38f7331263621b46a43f2
chenjingbang <2805478472@qq.com>
2022-04-05 10:33:52 +0800

    通过路由导航守卫控制访问权限

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -5,10 +5,24 @@ import Home from './components/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', name: 'login', component: Login },
     { path: '/home', name: 'home', component: Home }
   ]
 })
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 函数，next()代表放行  next('/login') 强制跳转
+
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+
+export default router
