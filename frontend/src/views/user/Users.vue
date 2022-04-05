15dd4915e95cc7ed744acbc7203ddc3540a95034
chenjingbang <2805478472@qq.com>
2022-04-05 22:26:50 +0800

    实现用户列表页基本布局，添加卡片视图区域，并为其增加一个搜索与添加区域

--- frontend/src/views/user/Users.vue
+++ frontend/src/views/user/Users.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
     <h1>这是用户列表</h1>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!--卡片视图区域-->
+    <el-card>
+      <el-row :gutter="24">
+        <el-col :span="8">
+          <!--搜索与添加区域-->
+          <el-input placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" @click="addDialogVisible = true">添加用户</el-button>
+        </el-col>
+      </el-row>
+    </el-card>
   </div>
 </template>
 
