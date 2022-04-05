f04f4bc680c878579995ded04b5c7d2ae7ce842a
chenjingbang <2805478472@qq.com>
2022-04-05 16:24:43 +0800

    通过axios请求拦截器添加token，保证拥有获取数据的权限

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,8 +10,15 @@ import './assets/css/global.css'
 // 导入字体图标
 import './assets/fonts/iconfont.css'
 
-// 设置请求根路径
+// 配置请求根路径
 axios.defaults.baseURl = 'http://127.0.0.1:8080/api'
+// 配置axios请求拦截器添加token，保证拥有获取数据的权限
+axios.interceptors.request.use(config => {
+  // 为请求头对象，添加 Token 验证的 Authorization 字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须return config
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
