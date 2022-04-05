2183d762317e0cae8b28e79d8db5595ef0c1f3b2
chenjingbang <2805478472@qq.com>
2022-04-05 22:11:44 +0800

    调整一下element.js的位置

--- frontend/src/plugins/element.js
+++ frontend/src/plugins/element.js
@@ -3,13 +3,15 @@ import {
   Button, Form, FormItem, Input, Message,
   Container, Header, Aside, Main,
   Menu, Submenu, MenuItem,
-  Breadcrumb, BreadcrumbItem, Card, Row, Col
+  Breadcrumb, BreadcrumbItem,
+  Card, Row, Col
 } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Button).use(Form).use(FormItem).use(Input)
 Vue.use(Container).use(Header).use(Aside).use(Main)
 Vue.use(Menu).use(Submenu).use(MenuItem)
-Vue.use(Breadcrumb).use(BreadcrumbItem).use(Card).use(Row).use(Col)
+Vue.use(Breadcrumb).use(BreadcrumbItem)
+Vue.use(Card).use(Row).use(Col)
 // 配置全局弹窗组件调用
 Vue.prototype.$message = Message
